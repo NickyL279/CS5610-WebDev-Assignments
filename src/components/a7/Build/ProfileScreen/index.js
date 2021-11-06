17717f19525fa3f60e7e073af2f14f74678ce2ad
NickyL279 <60162477+NickyL279@users.noreply.github.com>
2021-11-05 20:54:27 -0400

    a7 v4

--- src/components/a7/Build/ProfileScreen/index.js
+++ src/components/a7/Build/ProfileScreen/index.js
@@ -11,7 +11,7 @@ const ProfileScreen = () => {
     return (
         <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                <NavigationSidebar active="home"/>
+                <NavigationSidebar active="profile"/>
             </div>
             <div className="col-10 col-md-10 col-lg-7 col-xl-6" style={{"position": "relative"}}>
                 <ProfileComponent/>
